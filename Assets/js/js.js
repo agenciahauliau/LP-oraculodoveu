63b2a80a3df72f9b07ced9f16751b304d9e0b7b6
itamartcjr <itamar@hi4u.me>
2021-10-12 16:13:05 -0300

    0.0.0.4 - informações adicionadas

--- Assets/js/js.js
+++ Assets/js/js.js
@@ -3,9 +3,10 @@
 function efeitoScroll() {
     const MenuItens = document.querySelectorAll('.menu ul li')
     const sectionOne = document.querySelector('.section1')
+    const Menu = document.querySelector('.menu')
 
     MenuItens.forEach((item, idx) => {
-        if(sectionOne.getBoundingClientRect().bottom > item.getBoundingClientRect().bottom){
+        if (sectionOne.getBoundingClientRect().bottom > item.getBoundingClientRect().bottom) {
             item.classList.add('branco')
             item.classList.remove('preto')
         } else {
@@ -13,7 +14,16 @@ function efeitoScroll() {
             item.classList.remove('branco')
         }
     });
+
+    if (sectionOne.getBoundingClientRect().bottom < 0) {
+        Menu.classList.add('menuDesktop')
+    } else {
+        Menu.classList.remove('menuDesktop')
+    }
+
 };
+
+//////////// Ações
 
 efeitoScroll()
 
@@ -24,4 +34,15 @@ window.addEventListener('resize', function () {
 window.addEventListener('scroll', function () {
     efeitoScroll()
 });
+
+const enviarMensagem = document.querySelector('.form button')
+
+enviarMensagem.addEventListener('click', function () {
+    const nomeContato = document.querySelector('.form input[name="nome"]').value
+    const textoContato = document.querySelector('.form textarea[name="mensagem"]').value
+    
+    if (nomeContato && textoContato) {        
+        !window.open(`https://api.whatsapp.com/send?phone=+556281483910&text=Oi! meu nome é ${nomeContato}. ${textoContato}`)
+    }
+})
 
